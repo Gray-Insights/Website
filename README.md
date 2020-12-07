c2b6a8a45b93ad35a4c567663ee54274ae2e2ed4
unknown <jeffzhao7@gmail.com>
2020-12-07 12:36:05 -0500

    Show Image

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # NOTE
 tumor_model2.h5 is not included as it exceeds git's 100 mb limit.
-It has been uploaded herehttps://drive.google.com/file/d/1-Oln4XKmda2NQ0AjJ3tskLwQRtc7kyPI/view?usp=sharing
+It has been uploaded here: https://drive.google.com/file/d/1-Oln4XKmda2NQ0AjJ3tskLwQRtc7kyPI/view?usp=sharing
--- app.py
+++ app.py
@@ -7,10 +7,13 @@ import skimage.transform
 from tensorflow import keras
 from tensorflow.keras.preprocessing import image
 import numpy as np
-
+import copy
 app = flask.Flask(__name__, template_folder='templates')
 
 model = keras.models.load_model("tumor_model2.h5")
+
+UPLOAD_FOLDER = 'static/uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def callModel(img):
 
@@ -32,11 +35,14 @@ def main():
         file = flask.request.files['file']
 
         if file:
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename.replace(" ", "")))
             # Read the image using skimage
-            img = skimage.io.imread(file)
+
+            img = skimage.io.imread("static/uploads/"+str(filename).replace(" ", ""))
 
             # Resize the image to match the input the model will accept
-            img = skimage.transform.resize(img, (150, 150, 3))
+            img = skimage.transform.resize(img, (150, 150, 1))
 
             # Get prediction of image from classifier
             predictions = callModel(img)
@@ -44,10 +50,11 @@ def main():
             # Get the value of the prediction
             prediction = predictions[0]
 
-            return flask.render_template('index.html', prediction =  "<br><b>Giloma:</b> "+str(round(prediction[0]*100, 5))+"%" +
+
+            return flask.render_template('index.html', prediction =  "<b>Prediction for input image is</b> <br><b>Giloma:</b> "+str(round(prediction[0]*100, 5))+"%" +
                                                                      "<br><b>Meningioma:</b> "+str(round(prediction[1]*100, 5))+"%" +
                                                                      "<br><b>Pituitary:</b> "+str(round(prediction[3]*100, 5))+"%" +
-                                                                     "<br><b>No Tumor:</b> "+str(round(prediction[2]*100, 5))+"%")
+                                                                     "<br><b>No Tumor:</b> "+str(round(prediction[2]*100, 5))+"%", image="static/uploads/"+str(filename).replace(" ", ""))
 
     return(flask.render_template('index.html'))
 
